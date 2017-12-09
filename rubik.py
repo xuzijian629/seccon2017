0bcb42508c41fd442cd7c1b555121a340c9fd9d2
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-12-09 15:55:17 +0900

    add rubik.py

---
+++ rubik.py
@@ -0,0 +1,13 @@
+import urllib, urllib2
+import qrcode
+
+host = 'http://qubicrube.pwn.seccon.jp:33654/'
+img = 'images/01000000000000000000_B.png'
+
+import sys, qrcode
+
+d = qrcode.Decoder()
+if d.decode('out'):
+    print 'result: ' + d.result
+else:
+    print 'error: ' + d.error
