62514c7a20cc24cffb9f8b46545fb54bea4d4af2
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-12-09 16:36:32 +0900

    get next url

--- rubik.py
+++ rubik.py
@@ -3,15 +3,21 @@ from PIL import Image
 import zbarlight
 
 def get_file(f):
-    return f[7:-1] + f[-1]
+    return f[44:-1] + f[-1]
 
-host = 'http://qubicrube.pwn.seccon.jp:33654/'
-img = 'images/01000000000000000000_B.png'
+def generate_png_url(url):
+    suffix = ['_U', '_D', '_L', '_R', '_F', '_B']
+    return map(lambda s: url + s + '.png', suffix)
 
-urllib.urlretrieve(host + img, get_file(img))
-with open(get_file(img), 'rb') as image_file:
-    image = Image.open(image_file)
-    image.load()
+url = 'http://qubicrube.pwn.seccon.jp:33654/images/01000000000000000000'
 
-codes = zbarlight.scan_codes('qrcode', image)
-print('QR codes: %s' % codes)
+for _ in range(1):
+    for img in generate_png_url(url):
+        urllib.urlretrieve(img, get_file(img))
+        with open(get_file(img), 'rb') as image_file:
+            image = Image.open(image_file)
+            image.load()
+        code = zbarlight.scan_codes('qrcode', image)[0]
+        print code
+        if code[0:4] == 'http':
+            url = code.replace('http://qubicrube.pwn.seccon.jp:33654/', 'http://qubicrube.pwn.seccon.jp:33654/images/')
