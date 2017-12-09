9bad7d7f2c94a003f8b74d9d1cb05ab6859f5731
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2017-12-09 16:20:16 +0900

    can retrieve png

--- rubik.py
+++ rubik.py
@@ -1,11 +1,17 @@
 import urllib, urllib2
-import qrcode
+from PIL import Image
+import zbarlight
+
+def get_file(f):
+    return f[7:-1] + f[-1]
 
 host = 'http://qubicrube.pwn.seccon.jp:33654/'
 img = 'images/01000000000000000000_B.png'
 
-d = qrcode.Decoder()
-if d.decode('out'):
-    print 'result: ' + d.result
-else:
-    print 'error: ' + d.error
+urllib.urlretrieve(host + img, get_file(img))
+with open(get_file(img), 'rb') as image_file:
+    image = Image.open(image_file)
+    image.load()
+
+codes = zbarlight.scan_codes('qrcode', image)
+print('QR codes: %s' % codes)
